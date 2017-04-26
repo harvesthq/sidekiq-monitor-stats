660f25d8ce91dd2c6da5b7b9f4a11969f6fddf50
Albert Llop <mrsimo@gmail.com>
2017-04-26 10:00:24 +0200

    Handle sidekiq 5.0

--- lib/sidekiq/monitor/web.rb
+++ lib/sidekiq/monitor/web.rb
@@ -7,11 +7,17 @@ module Sidekiq
         app.get "/monitor-stats" do
           monitor_stats = Monitor::Stats.new
 
-          content_type :json
-          Sidekiq.dump_json(
+          data = {
             queues:    monitor_stats.queue_metrics,
             processes: monitor_stats.process_metrics
-          )
+          }
+
+          if Sidekiq::VERSION >= "5.0.0"
+            json(data)
+          else
+            content_type :json
+            Sidekiq.dump_json(data)
+          end
         end
       end
     end
